9c964a1a3a27e06b95a57caa057d2c9e9c65dcad
Chintan Vaghela <cheaterchintz@gmail.com>
2017-06-28 15:35:03 -0400

    Random Commit
    New GitHub Desktop

---
+++ SocietyMgmtApp/RoundedButton.swift
@@ -0,0 +1,37 @@
+//
+//  RoundedButton.swift
+//  SocietyMgmtApp
+//
+//  Created by Chintan Vaghela on 6/27/17.
+//  Copyright © 2017 CVBuilts. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+}
