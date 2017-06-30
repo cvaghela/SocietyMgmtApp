c2616b6b5f2296fc3624fa45bcc9dd5b55efad75
Chintan Vaghela <cheaterchintz@gmail.com>
2017-06-29 22:47:33 -0400

    June 29, 2017 10:46 pm
    Facebook and Google Login buttons added!

---
+++ SocietyMgmtApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocietyMgmtApp
+//
+//  Created by Chintan Vaghela on 6/29/17.
+//  Copyright © 2017 CVBuilts. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GREY: CGFloat = 120.0 / 255.0
--- SocietyMgmtApp/RoundedButton.swift
+++ SocietyMgmtApp/RoundedButton.swift
@@ -10,6 +10,18 @@ import UIKit
 
 @IBDesignable
 class RoundedButton: UIButton {
+    
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 20.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        
+    }
+    
     @IBInspectable var cornerRadius: CGFloat = 0 {
         didSet {
             layer.cornerRadius = cornerRadius
