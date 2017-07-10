830cda3deb00a8ded29642b2a1470c90a7de004f
Chintan Vaghela <cheaterchintz@gmail.com>
2017-07-10 03:05:30 -0400

    July 10, 2017 3:05 am
    New Page added for meetings!

---
+++ SocietyMgmtApp/AddSocietyVC.swift
@@ -0,0 +1,18 @@
+//
+//  AddSocietyVC.swift
+//  SocietyMgmtApp
+//
+//  Created by Chintan Vaghela on 7/8/17.
+//  Copyright © 2017 CVBuilts. All rights reserved.
+//
+
+import UIKit
+
+class AddSocietyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ SocietyMgmtApp/SocietyControlsVC.swift
@@ -0,0 +1,18 @@
+//
+//  SocietyControlsVC.swift
+//  SocietyMgmtApp
+//
+//  Created by Chintan Vaghela on 7/8/17.
+//  Copyright © 2017 CVBuilts. All rights reserved.
+//
+
+import UIKit
+
+class SocietyControlsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
